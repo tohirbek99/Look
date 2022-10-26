df94b19054e95452fcc71b96ab96bb39fed12efb
tohirbek99 <tohirbekarzimurodov@gmail.com>
2022-10-26 01:39:22 -0700

    Add project files.

---
+++ Look/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Look.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? PhoneNamber { get; set; }
+        public int OrderId { get; set; }
+        public virtual Order? Orders { get; set; }
+
+        public ICollection<FastFood>? FastFoods { get; set; }
+        public ICollection<Drink>? Drinks { get; set; }
+    }
+}
---
+++ Look/Models/FastFood.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Look.Models
+{
+    public class FastFood
+    {
+        [Key]
+        public int FoodId { get; set; }
+        public string? FoodName { get; set;}
+        public string? Price { get; set; }
+        
+        public int EmploeeId { get; set; }
+        public virtual Employee? Employees { get; set; }
+    }
+}
