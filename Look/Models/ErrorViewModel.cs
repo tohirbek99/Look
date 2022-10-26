0f808adc71281e447f7770768b7d8ed4943f17bf
tohirbek99 <tohirbekarzimurodov@gmail.com>
2022-10-26 01:41:16 -0700

    Revert "Add project files."
    This reverts commit 7864c36ef29cdba1a707804c9e4fad7f699cade0.

--- Look/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Look.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
