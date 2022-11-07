a49b8ac203201d87c9e4db92567eacb7a7b763b8
tohirbek99 <tohirbekarzimurodov@gmail.com>
2022-11-06 21:16:47 -0800

    1

---
+++ Look/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Look.DBContexts;
+using Look.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Look.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DBConnection _context;
+
+        public ProductsController(DBConnection context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int pg = 1)
+        {  
+
+            List<Product> products = _context.Products.ToList();
+            const int pageSize = 3;
+            if (pg < 1)
+            {
+                pg = 1;
+            }
+            int recsCount = products.Count();
+            var pager = new Pager(recsCount, pg, pageSize);
+            int recsSkip = (pg - 1) * pageSize;
+            var data = products.Skip(recsSkip).Take(pageSize).ToList();
+            this.ViewBag.Pager=pager;
+
+            return View(data);
+        }
+    }
+}
---
+++ Look/DBContexts/Components/CategoriesViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Look.DBContexts.Components
+{
+    public class CategoriesViewComponent:ViewComponent
+    {
+        private readonly DBConnection _context;
+
+        public CategoriesViewComponent(DBConnection context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+    }
+}
